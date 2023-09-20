efa2e0e7a315d8aa3f1d64e2d8a2d5f5590b3483
PietraVaezZ <pietravaezdasilva@gmail.com>
2023-09-20 18:02:58 -0300

    LH-Pets_MVC

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,7 +1,7 @@
 namespace LHPet.Models;
 public class Cliente
 {
-/* No trecho de código a seguir são criados os
+/* No trecho de código a seguir são criados os 
 atributos de Cliente e seus getters e setters
 correspondentes. */
 public int Id { get; set; }
@@ -9,10 +9,10 @@ public string Nome { get; set; }
 public string Cpf { get; set; }
 public string Email { get; set; }
 public string Paciente { get; set; }
-/* Nesse trecho do código, é criado o Construtor do
-model Cliente e determinado quais parâmetros são
+/* Nesse trecho do código, é criado o Construtor do 
+model Cliente e determinado quais parâmetros são 
 necessários na criação das instâncias. */
-public Cliente(int id, string nome, string cpf,
+public Cliente(int id, string nome, string cpf, 
 string email, string paciente)
 {
 this.Id = id;
